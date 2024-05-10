54f617e6a36846a4a22be43618e3860af8c0db00
Vladimir Malkhanov <github.com@finansist.com>
2024-05-10 17:16:58 +0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ templates_path = ['_templates']
 
 html_theme = 'sphinx_rtd_theme'
 html_css_files = [
-    'css/rtd_dark.css',
+    'css/rtd_dark2.css',
 ]
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
